01f45c2ff5d83a77be0ce903ec0f1c4e159342a9
micron <m1cron4900@gmail.com>
2021-03-14 15:39:31 +0300

    task 2.3 done

---
+++ 2/23.py
@@ -0,0 +1,24 @@
+def f23(table):
+    res = []
+    for sublist in table:
+        cleaned = [elem for elem in sublist if elem is not None]
+        if len(cleaned):
+            res.append(cleaned)
+    for row in res:
+        row[1] = "{:.0f}%".format(float(row[1]) * 100)
+        tmp = row[0].split("|")
+        row[0] = tmp[1]
+        date = tmp[0].split('.')
+        row.append("{}.{}.{}".format(date[2], date[1], date[0]))
+    return res
+
+print(
+    f23([
+            [None, "13.01.2004|Кулянц, Д.А.", "0.9"],
+            [None, None, None],
+            [None, "10.04.2003|Мевов, П.Б.", "0.8"],
+            [None, None, None],
+            [None, "01.09.1999|Фекумян, В.А.", "0.4"],
+            [None, "07.09.2004|Сусий, В.Б.", "0.4"]
+        ])
+)
